083e0d41a65b2827891f1dd7ebfc96aea81ce420
Kouhei <kouhei.k.0116@gmail.com>
2019-03-28 22:56:16 +0900

    Update files

--- App.js
+++ App.js
@@ -5,97 +5,18 @@ import {
   Text,
   View,
   FlatList,
-  Dimensions
+  Dimensions,
+  Linking
 } from 'react-native';
-import TodoInput from './src/component/TodoInput';
-import TodoItem from './src/component/TodoItem';
-import Topic from './src/drawer/Topic';
-import Connect from './src/drawer/Connect';
-import HomeScreen from './src/drawer/Home';
+
 import Icon from 'react-native-vector-icons/FontAwesome'
 import {Header, Button} from 'react-native-elements';
 import {createDrawerNavigator, DrawerItems, createSwitchNavigator, createStackNavigator} from 'react-navigation';
-
+import roslib from 'roslib';
+import BackgroundFetch from "react-native-background-fetch";
 
 const SCREEN_WIDTH = Dimensions.get('window').width;
 
-const ProfileScreen = () => (
-  <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-    <Text>Profile Screen</Text>
-        <Button
-      backgroundColor='#03A9F4'
-      buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
-      title='Check screenProps'
-      onPress = {() => alert(Object.keys(this.props.navigation))} />
-  </View>
-);
-
-const CustomDrawerContentComponent = props => (
-  <View style={{ flex: 1, backgroundColor: '#43484d' }}>
-    <View style={{ marginTop: 40, justifyContent: 'center', alignItems: 'center' }}>
-    </View>
-    <View style={{ marginLeft: 10 }}>
-      <DrawerItems {...props} />
-    </View>
-  </View>
-);
-
-const Drawer = createDrawerNavigator({
-  
-    Home: {
-      screen: HomeScreen//({navigation}) => <HomeScreen screenProps={{drawerNavigation:navigation}}/>
-    },
-    
-    Topic: {
-      screen:Topic
-      //({screenProps}) => <Topic 
-      //  {...props}
-      //  screenProps={{drawerNavigation:navigation}}/>
-      //  screenProps = {screenProps} />
-    },
-    Profile: {
-      screen:ProfileScreen//({navigation}) => <ProfileScreen screenProps={{drawerNavigation:navigation}}/>
-    },
-  },
-  {
-    initialRouteName: 'Topic',
-    contentOptions: {
-      activeTintColor: '#548ff7',
-      activeBackgroundColor: 'transparent',
-      inactiveTintColor: '#ffffff',
-      inactiveBackgroundColor: 'transparent',
-      labelStyle: {
-        fontSize: 15,
-        marginLeft: 0,
-      },
-    },
-    drawerWidth: SCREEN_WIDTH * 0.6,
-    contentComponent: CustomDrawerContentComponent,
-    drawerOpenRoute: 'DrawerOpen',
-    drawerCloseRoute: 'DrawerClose',
-    drawerToggleRoute: 'DrawerToggle',
-  }
-);
-
-Drawer.navigationOptions = ({ navigation }) => {
-  return {
-    
-    headerLeft: 
-        { icon: 'menu', 
-          color: '#fff',
-        }
-          //onPress: () => navigation.navigate('DrawerToggle')
-    }
-};
-
-const RootNavigator = createSwitchNavigator({
-    Connect: {screen: Connect},
-    Connected: {screen: Drawer},
-  },
-  {
-    initialRouteName: 'Connect',
-  }
-);
 
 
 export default class App extends Component<{}> {
@@ -107,98 +28,73 @@ export default class App extends Component<{}> {
     };
   }
   
-  _delete = (index) => () => {
-    const list = [].concat(this.state.list);
-    list.splice(index, 1);
-
-    this.setState({
-      list,
+  componentDidMount(){
+    const ros = new roslib.Ros({
+      url: 'ws://192.168.4.93:9090' 
+    });
+    ros.on('connection',() => {
+        alert('Connected to rosbridge server');
+    });
+    ros.on('error', error => {
+        alert('Error connecting to rosbridge server');
+    });
+    ros.on('close', () => {
+      alert('Connection to rosbridge server was closed');
+    });
+    
+    const skype_server = new roslib.Service({
+      ros: ros,
+      name: 'skype_server',
+      serviceType: "tms_rc_double/skype_srv",
+    });
+    //ROSのサービスを生成
+    skype_server.advertise((req,res) => {
+      try{
+          let url = 'skype:' + req.id + '?call&amp;video=true';
+          //IDから音声+ビデオ通話を発信するURLを生成
+          alert(url);
+          Linking.openURL(url);
+          res.success = 1;
+      }catch(e){
+          alert(e);
+          res.success = 0;
+      }
+    });
+  
+    // Optional: Query the authorization status.
+    BackgroundFetch.status((status) => {
+      switch(status) {
+        case BackgroundFetch.STATUS_RESTRICTED:
+          console.log("BackgroundFetch restricted");
+          break;
+        case BackgroundFetch.STATUS_DENIED:
+          console.log("BackgroundFetch denied");
+          break;
+        case BackgroundFetch.STATUS_AVAILABLE:
+          console.log("BackgroundFetch is enabled");
+          break;
+      }
     });
   }
-
-  _done = (index) => () => {
-    const list = [].concat(this.state.list);
-    list[index].done = !list[index].done;
-
-    this.setState({
-      list,
-    });
-  }
-
-  _onPress = (text) => {
-    const list = [].concat(this.state.list);
-
-    list.push({
-      key: Date.now(),
-      text: text,
-      done: false,
-    });
-
-    this.setState({
-      list,
-    });
-  }
+  
   render() {
     const {
       list,
     } = this.state;
     return (
-      /*
-      <View style={styles.container}>
-        <Header
-          leftComponent={{ icon: 'menu', color: '#fff' }}
-          centerComponent={{ text: 'Todo List', style: { color: '#fff' } }}
-          rightComponent={{ icon: 'home', color: '#fff' }}
-          outerContainerStyles={{height: 53}}
-        />
-        <View style={styles.main}>
-          <TodoInput onPress={this._onPress} />
-          <View style={styles.todoListContainer}>
-            <FlatList
-              style={styles.todoList}
-              data={list}
-              renderItem={({ item, index }) => (
-                <TodoItem 
-                  onDone={this._done(index)}
-                  onDelete={this._delete(index)}
-                  {...item}
-                />
-              )}
-            />
-          </View>
-        </View>
-      </View>
-      */
-     <Drawer screenProps={5}/>
+
+     <View style={styles.container}>
+  </View>
     );
   }
 }
 
 
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#eee',
+    backgroundColor: '#000',
     //alignItems: 'center',
   },
-  main: {
-    flex: 1,
-    maxWidth: 400,
-    alignItems: 'center',
-  },
-  todoListContainer: {
-    flexDirection: 'row',
-    flex: 1,
-  },
-  todoList: {
-    paddingLeft: 10,
-    paddingRight: 10,
-  },
-  Title:{
-    fontSize: 30,
-    //fontFamily:'Roboto',
-    margin: 20,
-    fontStyle: 'italic',
-    fontWeight: 'bold'
-  }
 });
--- README.md
+++ README.md
@@ -1,1 +1,73 @@
-reactROSApp
+## reactROSApp
+
+### 注意
+もともとこのアプリはReact Nativeの勉強のために作っていたアプリをデモ用に切り替えたものです。
+そのため不必要な部分が混ざっていたり(importのあたりとか)、挙動が不安定な部分があります。
+
+現在わかっている改善が必要な点
+* アプリが起動したあとしばらくするとサービスコールを受けなくなる。
+* 一度サービスコールを受けてアプリが切り替わる(このアプリの状態がBackgroundに遷移する)と、サービスが停止される。
+
+
+### 環境構築
+* ROSと`rosbridge_server`をインストールしたPC
+* React Nativeの開発環境
+React Nativeの環境構築についてはウェブ上に記事がたくさんあると思うのでそちらを見てください。
+自分がインストールしていたのは以下のバージョンでした。
+```
+    react-native-cli: 2.0.1
+    react-native: 0.55.4
+```
+### 説明
+roslibjsを使ってrosbridgeを介してROSと接続し、
+
+    const ros = new roslib.Ros({
+      url: 'ws://192.168.4.93:9090' 
+    });
+    
+ROSのサービスコールを受けると引数に指定されたskype IDへビデオ通話を発信します。
+
+    const skype_server = new roslib.Service({
+      ros: ros,
+      name: 'skype_server',
+      serviceType: "tms_rc_double/skype_srv",
+    });
+    skype_server.advertise((req,res) => {
+      try{
+          let url = 'skype:' + req.id + '?call&amp;video=true';
+          alert(url);
+          Linking.openURL(url);
+          res.success = 1;
+      }catch(e){
+          alert(e);
+          res.success = 0;
+      }
+    });
+
+Skypeをインストール済みの端末ではurlを利用して通話の発信ができるため、今回は引数からurlを生成し、音声通話とビデオ通話をtrueにしてリンクへアクセスしています(詳しい説明は公式のチュートリアル等を見てください)。
+
+機能を必要最小限にしたため、背景を黒にして何も表示させていません。(サービスコールを受けるとアラートは出ます)
+
+    const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#000',
+        //alignItems: 'center',
+    },
+    });
+
+roslibjsの使い方がわからない場合は
+以前自分が書いた[こちら](https://qiita.com/kouhei-k/items/bb44c070d4304caa4bf1)の記事、もしくは[ROS Wiki](http://wiki.ros.org/roslibjs)を見てください(Wikiはサービス周りの説明が少し少ないです)。
+
+React Nativeを使って開発しているのでAndroid, iOSのどちらも対応可能ですが、Androidでしか動作は確認していません。
+
+### 手順
+1. PC上で`roslaunch rosbridge_server rosbridge_websocket`を実行
+2. このアプリを起動
+3. サービスコールを発行
+4. アプリにアラートが表示されるとともにSkypeに画面が切り替わり、通話が発信される
+5. 相手の端末で受信、この際応答しただけでは音声通話のみなので相手の端末側でビデオ通話をオンにする。
+
+
+
+
